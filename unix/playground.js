c7c1b306ea647009b5b0f160092b9c5176ca5669
purnapattela <purnapattela@gmail.com>
2025-10-07 12:38:59 +0530

    Day-17 started unix concepts in nodejs

---
+++ unix/playground.js
@@ -0,0 +1,17 @@
+const { spawn, exec } = require("node:child_process");
+
+// const subprocess = spawn("ls", ["-la"])
+
+// subprocess.stdout.on("data", (data) => {
+//     console.log(data.toString("utf-8"));
+// })
+
+// exec("ls -la", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`Error : `, error)
+//         return;
+//     }
+//     console.log(`stdout of ll: `, stdout)
+//     console.log(`stderr of ll: `, stderr)
+// })
+
